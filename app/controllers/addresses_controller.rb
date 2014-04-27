5ffd40b3f01c709a3ed9d163efea2cf23de36003
jmontuiuc <j-monton@kellogg.northwestern.edu>
2014-04-26 22:00:13 -0500

    Added code to display correct lat and long

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -7,10 +7,14 @@ class AddressesController < ApplicationController
     @url_safe_address = URI.encode(@address)
 
     # Your code goes here.
-    # url = ?
-    # raw_data = ?
-    # parsed_data = ?
-    # @latitude = ?
-    # @longitude = ?
+    url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{@url_safe_address}&sensor=false"
+    raw_data = open(url).read
+    parsed_data = JSON.parse(raw_data)
+    results = parsed_data["results"]
+    first = results[0]
+    geometry = first["geometry"]
+    location = geometry["location"]
+    @latitude = location["lat"]
+    @longitude = location["lng"]
   end
 end
